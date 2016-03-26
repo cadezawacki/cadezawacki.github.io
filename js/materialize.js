6f96a9538fd2341d45b35f82eb9dc611cb51f6ea
cadezawacki <zawacki.cade@gmail.com>
2016-03-26 03:54:46 -0400

    edit

--- js/materialize.js
+++ js/materialize.js
@@ -444,7 +444,7 @@ if ($) {
 
   $(document).ready(function(){
   	$(window).scroll(function() {
-		  if( $(this).scrollTop() > $('body').height() - 80 ) { //
+		  if( $(this).scrollTop() > $('header').height() ) { //
 		    $(".main-nav").addClass(("main-nav-scrolled"));
 		  } else {
 		    $(".main-nav").removeClass(("main-nav-scrolled"));
